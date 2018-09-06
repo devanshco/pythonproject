aa99241e46119e6d53434a9a47ce13df4d8e44a0
Devansh Arora <developer.devansh@gmail.com>
2018-09-06 13:33:56 +0530

    ex15 file

---
+++ ex15.py
@@ -0,0 +1,21 @@
+# Importing Argument Variable Module
+from sys import argv
+
+# Getting Values for Argument Variable
+script, filename = argv
+
+# Variable To Look for the file
+txt = open(filename)
+
+# Print Commands to print the filename and Variable Content ie the file Content
+print(f"Here's your file {filename}:")
+print(txt.read())
+
+# Print Command and Variable to get user input
+print("Type he filename again:")
+file_again = input(">> ")
+
+
+txt_again = open(file_again)
+
+print(txt_again.read())
